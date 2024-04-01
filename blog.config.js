3af4cf3b4a47eefa96ab12faf606d50d5193ed07
阿豪 <nolankuang@gmail.com>
2024-04-01 23:50:34 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -22,8 +22,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Nolan', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '靠天吃饭自由人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://nolanblog.top', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion,Nolan,博客,阿豪', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
